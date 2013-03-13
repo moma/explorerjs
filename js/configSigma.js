6ee646b3ef4d44fc23a3bcda5e48d372fd4a3b82
PkSM3 <pokesam3@gmail.com>
2013-03-13 06:22:06 -0300

    restructured code: new sigma.gexf

--- js/configSigma.js
+++ js/configSigma.js
@@ -5,6 +5,61 @@ var minLengthAutoComplete = 1;
 var maxSearchResults = 10;
 var strSearchBar = "Search";
 
+
+var zoom=0;
+var a_node_size= 0.50;
+var b_node_size= 0.50;
+var cursor_size= 0.00;
+var a_edge_filter_min= 0.0;
+var a_edge_filter_max= 1.0;
+var a_node_filter_min= 0.0;
+var a_node_filter_max= 1.0;
+var b_edge_filter_min= 0.0;
+var b_edge_filter_max= 1.0;
+var b_node_filter_min= 0.0;
+var b_node_filter_max= 1.0;
+
+var checkBox=false;
+var overNodes=false;
+
+var socsemFlag=false;
+var constantNGramFilter;
+var socialFlag=true;
+var semanticFlag=false;
+var maxNodeSize;
+var minNodeSize;
+var sliderNodeWeight;
+var maxEdgeWeight;
+var minEdgeWeight;
+
+var overviewWidth = 200;
+var overviewHeight = 175;
+var overviewScale = 0.25;
+var minZoom = 1;
+var maxZoom = 7;
+var moveDelay = 80, zoomDelay = 2;
+//var Vecindad;
+var gexf;
+var partialGraph;
+var labels = [];    
+var Nodes = []; 
+var Edges = [];
+
+var selections = [];
+var opossites = {};
+
+var matches = [];
+
+var nodes1 = [];
+var nodes2 = [];
+var bipartiteD2N = [];
+var bipartiteN2D = [];
+
+var flag=0;
+var firstime=0;
+var leftright=true;
+var edgesTF=false;
+
 var sigmaJsDrawingProperties = {
     defaultLabelColor: 'black',
     defaultLabelSize: 14,
