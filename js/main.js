3d305bc0b487028e2e4f8b95ca7fe7db441397c0
PkSM3 <pokesam3@gmail.com>
2013-06-27 14:03:14 +0200

    one partite almost finished

--- js/main.js
+++ js/main.js
@@ -37,6 +37,7 @@ function startOnePartite(pathfile) {
     $("#category-B").hide();
     $("#images").hide();
     
+    pr("in one partite net");    
     pr("pathfile: "+pathfile);
     partialGraph = sigma.init(document.getElementById('sigma-example'))
     .drawingProperties(sigmaJsDrawingProperties)
@@ -143,129 +144,6 @@ function startOnePartite(pathfile) {
         return false;
     });
     
-}
-
-function startBipartite(pathfile) {
-    //currentUrl=window.location.href;
-    pr("pathfile: "+pathfile);
-    partialGraph = sigma.init(document.getElementById('sigma-example'))
-    .drawingProperties(sigmaJsDrawingProperties)
-    .graphProperties(sigmaJsGraphProperties)
-    .mouseProperties(sigmaJsMouseProperties);
-    
-    partialGraph.ctxMini = document.getElementById('overview').getContext('2d'); 
-    partialGraph.ctxMini.clearRect(0, 0, overviewWidth, overviewHeight);
-    partialGraph.totalScroll=0;    
-    partialGraph.centreX = partialGraph._core.width/2;
-    partialGraph.centreY = partialGraph._core.heigth/2;
-       
-    $('#sigma-example').css('background-color','white');
-    $("#category-B").hide();
-    $("#labelchange").hide();
-    $("#availableView").hide();
-    
-    console.log("parsing...");        
-    parse(pathfile);
-    fullExtract(); 
-    updateEdgeFilter("social");
-    pushSWClick("social");
-    console.log("Parsing complete.");
-    /*======= Show some labels at the beginning =======*/
-    minIn=50,
-    maxIn=0,
-    minOut=50,
-    maxOut=0;        
-    partialGraph.iterNodes(function(n){
-        if(parseInt(n.inDegree) < minIn) minIn= n.inDegree;
-        if(parseInt(n.inDegree) > maxIn) maxIn= n.inDegree;
-        if(parseInt(n.outDegree) < minOut) minOut= n.outDegree;
-        if(parseInt(n.outDegree) > maxOut) maxOut= n.outDegree;
-    });
-    partialGraph.iterNodes(function(n){
-        if(n.inDegree==minIn) n.forceLabel=true;
-        if(n.inDegree==maxIn) n.forceLabel=true;
-        if(n.outDegree==minOut) n.forceLabel=true;
-        if(n.outDegree==maxOut) n.forceLabel=true;
-    });
-    /*======= Show some labels at the beginning =======*/
-    
-    
-    partialGraph.zoomTo(partialGraph._core.width / 2, partialGraph._core.height / 2, 0.8).draw();
-    initializeMap();
-    updateMap();
-        
-    //window.onhashchange = updateMap;
-    
-    updateDownNodeEvent(false);
-        
-    /* Initial Effect (Add: unchecked) HIDE */
-    partialGraph.bind('overnodes',function(event){ 
-        var nodes = event.content;
-        var neighbors = {};
-        var nrEdges = 0;
-        var e = partialGraph._core.graph.edges;
-        for(i=0;i<e.length;i++){
-            if(nodes.indexOf(e[i].source.id)>=0 || nodes.indexOf(e[i].target.id)>=0){
-                neighbors[e[i].source.id] = 1;
-                neighbors[e[i].target.id] = 1;
-                nrEdges++;//github.com/jacomyal/sigma.js/issues/62
-            }
-        }
-        //partialGraph.draw(2,1,2);
-        partialGraph.iterNodes(function(n){
-            if(nrEdges>0) {
-                if(!neighbors[n.id]){
-                    n.hidden = 1;
-                }else{
-                    n.hidden = 0;
-                }
-            }
-        }).draw(2,1,2);
-    });
-  
-    partialGraph.bind('outnodes',function(){
-        var e = partialGraph._core.graph.edges;
-        for(i=0;i<e.length;i++){
-            e[i].hidden = 0;
-        }
-        partialGraph.draw(2,1,2);
-            
-        partialGraph.iterNodes(function(n){
-            n.hidden = 0;
-        }).draw(2,1,2);
-    });
-    /* Initial Effect (Add: unchecked) HIDE */
-    
-    partialGraph.startForceAtlas2();
-    //partialGraph.draw();    
-    
-    $("#loading").remove();
-    
-    $("#aUnfold").click(function() {
-        var _cG = $("#leftcolumn");
-        if (_cG.offset().left < 0) {
-            _cG.animate({
-                "left" : "0px"
-            }, function() {
-                $("#aUnfold").attr("class","leftarrow");
-                $("#zonecentre").css({
-                    left: _cG.width() + "px"
-                });
-            }); 
-        } else {
-            _cG.animate({
-                "left" : "-" + _cG.width() + "px"
-            }, function() {
-                $("#aUnfold").attr("class","rightarrow");
-                $("#zonecentre").css({
-                    left: "0"
-                });
-            });
-        }
-        return false;
-    });
-    
-        
     
     /******************* /SEARCH ***********************/
     $.ui.autocomplete.prototype._renderItem = function(ul, item) {
@@ -374,20 +252,7 @@ function startBipartite(pathfile) {
     //    .mouseup(endMove)
     //    .mouseout(endMove)
     .mousewheel(onGraphScroll);
-    
-    $("sigma-example")
-    //    .mousemove(onOverviewMove)
-    //    .mousedown(startMove)
-    //    .mouseup(endMove)
-    //    .mouseout(endMove)
-    //    .mousewheel(onGraphScroll); -> it doesn't answer!
-    
-    //    $("#cancelselection").click(function (){
-    //        pr("heeeeree");
-    //        cancelSelection();
-    //    });
-    
-    $("#zoomPlusButton").click(function () {
+        $("#zoomPlusButton").click(function () {
         partialGraph.zoomTo(partialGraph._core.width / 2, partialGraph._core.height / 2, partialGraph._core.mousecaptor.ratio * 1.5);
         $("#zoomSlider").slider("value",partialGraph.position().ratio);
         return false;
@@ -418,30 +283,15 @@ function startBipartite(pathfile) {
         slide: function(event, ui) {
             $.doTimeout(300,function (){
                 partialGraph.iterNodes(function (n) {
-                    if(n.id.charAt(0)=="D") {
+                    //if(n.id.charAt(0)=="D") {
                         n.size = parseFloat(Nodes[n.id].size) + parseFloat((ui.value-1))*0.3;
-                    }
+                    //}
                 });
                 partialGraph.draw();
             });
         }
     });
-    $("#sliderBNodeSize").slider({
-        value: 1,
-        min: 1,
-        max: 25,
-        animate: true,
-        slide: function(event, ui) {
-            $.doTimeout(300,function (){
-                partialGraph.iterNodes(function (n) {
-                    if(n.id.charAt(0)=="N") {
-                        n.size = parseFloat(Nodes[n.id].size) + parseFloat((ui.value-1))*0.3;
-                    }
-                });
-                partialGraph.draw();
-            });
-        }
-    });
+    
     $("#sliderSelectionZone").slider({
         value: cursor_size * 5.0,
         min: 0.0,
@@ -454,9 +304,7 @@ function startBipartite(pathfile) {
         //return callSlider("#sliderSelectionZone", "selectionRadius");
         }
     });
-    
 }
-
 
 function startBipartite(pathfile) {
     //currentUrl=window.location.href;
