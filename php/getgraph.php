33ddc545fc69bffb2a7a62c8a64e44c00675572a
PkSM3 <pokesam3@gmail.com>
2013-07-10 12:45:17 +0200

    these are the correct files

--- php/getgraph.php
+++ php/getgraph.php
@@ -28,8 +28,7 @@ function is_utf8($string) {
 }
 
 //phpinfo();
-$gexf = '';
-$edgesXML='';
+$gexf = '<?xml version="1.0" encoding="UTF-8"?>';
 $data = json_decode($_GET['query']);
 
 
@@ -179,7 +178,6 @@ $terms_colors = array(); // pour dire s'il y a des jobs postés sur ce term
 #echo "END;";
 foreach ($base->query($sql) as $row) {
     $info = array();
-    $info['id'] = $row['id'];
     $info['unique_id'] = $row['unique_id'];
     $info['photo_url'] = $row['photo_url'];
     $info['first_name'] = $row['first_name'];
@@ -208,154 +206,6 @@ foreach ($base->query($sql) as $row) {
 //echo "<html>____".sizeof($scholars)."</html>";
 include('gexf_generator.php');
 
-
-exec("rm -R gexfs/*");
-$showdate = date('Y-m-d_H:i:s') . "." . microtime_float();
-$handle = fopen('gexfs/' . $showdate . '.gexf', "w", "UTF-8");
-fputs($handle, $gexf);
-fclose($handle);
-$gexf=NULL;
-//echo date('Y-m-d_H:i:s').": Writing gexf DONE<br>";
-//echo date('Y-m-d_H:i:s').": Calling jar...<br>";
-exec("/usr/bin/java -jar tinaviz-2.0-SNAPSHOT.jar gexfs/$showdate.gexf 70", $outputo);
-//echo date('Y-m-d_H:i:s').": output = ".$outputo[0];
-
-$xml = simplexml_load_file($outputo[0]);
-$xml_array = object2array($xml);
-$newpositions=array();
-foreach ($xml_array['graph']['nodes'] as $nodes) {
-    for ($i = 0; $i < count($nodes); $i++) {
-        $info = array();
-        $info['x'] = $nodes[$i] ["@attributes"]['x'];
-        $info['y'] = $nodes[$i] ["@attributes"]['y']; 
-        $newpositions[$nodes[$i]["@attributes"]['id']]=$info;
-    }
-    break;
-}
-
-$gexf='';
-$gexf .= '<gexf xmlns="http://www.gexf.net/1.1draft" xmlns:viz="http://www.gephi.org/gexf/viz" version="1.1"> ';
-$gexf .= '<meta lastmodifieddate="20011-11-11">'."\n";
-$gexf .= ' </meta>'."\n";
-$gexf .= '<graph type="static">' . "\n";
-$gexf .= '<attributes class="node" type="static">' . "\n";
-$gexf .= ' <attribute id="0" title="category" type="string">  </attribute>' . "\n";
-$gexf .= ' <attribute id="1" title="occurrences" type="float">    </attribute>' . "\n";
-$gexf .= ' <attribute id="2" title="content" type="string">    </attribute>' . "\n";
-$gexf .= ' <attribute id="3" title="keywords" type="string">   </attribute>' . "\n";
-$gexf .= ' <attribute id="4" title="weight" type="float">   </attribute>' . "\n";
-$gexf .= '</attributes>' . "\n";
-$gexf .= '<attributes class="edge" type="float">' . "\n";
-$gexf .= ' <attribute id="5" title="cooc" type="float"> </attribute>' . "\n";
-$gexf .= ' <attribute id="6" title="type" type="string"> </attribute>' . "\n";
-$gexf .= "</attributes>" . "\n";
-$gexf .= "<nodes>" . "\n";
-foreach ($terms_array as $term) {
-	$nodeId = 'N::' . $term['id'];
-	$nodeLabel = str_replace('&', ' and ', $terms_array[$term['id']]['term']);
-	$gexf .= '<node id="' . $nodeId . '" label="' . $nodeLabel . '">' . "\n";
-	$gexf .= '<viz:color b="19" g="'.max(0,180-(100*$terms_colors[$term['id']])).'"  r="244"/>' . "\n";
-	$gexf .= '<viz:position x="'.($newpositions['N::' . $term['id']]['x']).'" y="'.($newpositions['N::'.$term['id']]['y']) . '"  z="0" />' . "\n";
-	$gexf .= '<attvalues> <attvalue for="0" value="NGram"/>' . "\n";
-	$gexf .= '<attvalue for="1" value="' . $terms_array[$term['id']]['occurrences'] . '"/>' . "\n";
-	$gexf .= '<attvalue for="4" value="' . $terms_array[$term['id']]['occurrences'] . '"/>' . "\n";
-	$gexf .= '</attvalues></node>' . "\n";
-}
-
-
-foreach ($scholars as $scholar) {
-    $uniqueId = $scholar['unique_id'];
-    if (!array_key_exists($uniqueId, $scholarsMatrix)) {
-        continue;
-    }
-    if (count($scholarsMatrix[$uniqueId]['cooc']) >= $min_num_friends) {
-        $nodeId = 'D::' . $uniqueId;
-        $theID = $scholar['id'];
-        $nodeLabel = $scholar['title'] . ' ' . $scholar['first_name'] . ' ' . $scholar['initials'] . ' ' . $scholar['last_name'];
-        $content = '';        
-        if ($scholar['photo_url'] != null) {
-            $content .= '<img  src=http://main.csregistry.org/' . $scholar['photo_url'] . ' width=' . $imsize . 'px  style=float:left;margin:5px>';
-        } else {
-            if (count($scholars) < 2000) {
-                $im_id = floor(rand(0, 11));
-                $content .= '<img src=http://communityexplorer.csregistry.org/img/' . $im_id . '.png width=' . $imsize . 'px   style=float:left;margin:5px>';
-            }
-        }
-
-        $content .= '<b>Country: </b>' . $scholar['country'] . '</br>';
-
-        if ($scholar['position'] != null) {
-            $content .= '<b>Position: </b>' . str_replace('&', ' and ', $scholar['position']) . '</br>';
-		}
-		$affiliation = '';
-		if ($scholar['lab'] != null) {
-			$affiliation .= $scholar['lab'] . ',';
-		}
-		if ($scholar['affiliation'] != null) {
-			$affiliation .= $scholar['affiliation'];
-		}
-		if (($scholar['affiliation'] != null) | ($scholar['lab'] != null)) {
-			$content .= '<b>Affiliation: </b>' . str_replace('&', ' and ', $affiliation) . '</br>';
-		}
-
-		if (strlen($scholar['keywords']) > 3) {
-			$content .= '<b>Keywords: </b>' . str_replace(',', ', ', substr($scholar['keywords'], 0, -1)) . '.</br>';
-		}
-
-		if (substr($scholar['homepage'], 0, 3) === 'www') {
-			$content .= '[ <a href=' . str_replace('&', ' and ', 'http://' . $scholar['homepage']) . ' target=blank > View homepage </a ><br/>]';
-		} elseif (substr($scholar['homepage'], 0, 4) === 'http') {
-			$content .= '[ <a href=' . str_replace('&', ' and ', $scholar['homepage']) . ' target=blank > View homepage </a >]<br/>';
-		}
-		if ($scholars_colors[$scholar['login']] ==1) {
-			$color = 'b="243" g="183"  r="19"';
-		} elseif (strcmp ($scholar['job_market'],'Yes')==0){
-                        $color = 'b="139" g="28"  r="28"';
-                }else{
-			$color = 'b="78" g="193"  r="127"';
-		}
-                
-                if (is_utf8($nodeLabel)) {
-                        $gexf .= '<node id="D::' . $theID. '" label="' . $nodeLabel . '">' . "\n";
-			//$gexf .= '<viz:color b="'.(243-min(243,(200*$scholars_colors[$scholar['login']]))).'" g="183"  r="19"/>' . "\n";
-			$gexf .= '<viz:color '.$color.'/>' . "\n";
-			$gexf .= '<viz:position x="' . ($newpositions['D::' .$theID]['x']) . '"    y="' . ($newpositions['D::' .$theID]['y']) . '"  z="0" />' . "\n";
-			$gexf .= '<attvalues> <attvalue for="0" value="Document"/>' . "\n";
-			if (true) {
-				$gexf .= '<attvalue for="1" value="12"/>' . "\n";
-				$gexf .= '<attvalue for="4" value="12"/>' . "\n";
-
-			} else {
-				$gexf .= '<attvalue for="1" value="10"/>' . "\n";
-				$gexf .= '<attvalue for="4" value="10"/>' . "\n";
-
-			}
-			if (is_utf8($content)) {
-				$gexf .= '<attvalue for="2" value="' . htmlspecialchars($content) . '"/>' . "\n";
-			}
-			$gexf .= '</attvalues></node>' . "\n";
-		}
-	}
-
-}
-$gexf .= "</nodes>" . "\n";
-
-$gexf .= "<edges>" . "\n";
-$gexf .=" ".$edgesXML;
-$gexf .= "</edges>" . "\n";
-
-$gexf .= "</graph>" . "\n";
-$gexf .= "</gexf>" . "\n";
-echo $gexf;
-
-function object2array($object) {
-    return @json_decode(@json_encode($object), 1);
-}
-
-function microtime_float() {
-    list($usec, $sec) = explode(" ", microtime());
-    return ((float) $usec + (float) $sec);
-}
 function objectToArray($d) {
     if (is_object($d)) {
         // Gets the properties of the given object
@@ -395,4 +245,6 @@ function scholarlink($term_occurrences, $scholars1_nb_keywords, $scholars2nb_key
         return 0;
     }
 }
+
+echo "$gexf";
 ?>
