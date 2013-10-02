45c454648fc7ec180ba40aecd9ee7b3270d5a196
PkSM3 <pokesam3@gmail.com>
2013-10-02 12:38:51 +0200

    fa2.js->fa2.py: ForceFactory finished

--- php/FA2.py/ForceFactory.py
+++ php/FA2.py/ForceFactory.py
@@ -50,8 +50,6 @@ class linRepulsion:
 				n2["fa2"]["dx"] -= xDist * factor
 				n2["fa2"]["dy"] -= yDist * factor
 
-
-
 	def apply_nr(self,n,r)
 		#not used?
 		'''
@@ -59,7 +57,7 @@ class linRepulsion:
 		yDist = n["y"] - r.config('massCenterY')
 		distance = math.sqrt(xDist * xDist + yDist * yDist)
 		if (distance > 0):
-			factor = self.coefficient * n["fa2"]["mass"] * r.config('mass') /Math.pow(distance, 2)
+			factor = self.coefficient * n["fa2"]["mass"] * r.config('mass') /math.pow(distance, 2)
 			n["fa2"]["dx"] += xDist * factor
 			n["fa2"]["dy"] += yDist * factor
 		'''
@@ -94,28 +92,196 @@ class linRepulsion_antiCollision:
 				n2["fa2"]["dx"] -= xDist * factor
 				n2["fa2"]["dy"] -= yDist * factor
 			elif (distance < 0):
-				factor = 100 * self.coefficient * n1["fa2"]["mass"] * n2["fa2"]["mass"];
+				factor = 100 * self.coefficient * n1["fa2"]["mass"] * n2["fa2"]["mass"]
 				n1["fa2"]["dx"] += xDist * factor
 				n1["fa2"]["dy"] += yDist * factor
 				n2["fa2"]["dx"] -= xDist * factor
 				n2["fa2"]["dy"] -= yDist * factor
 
-	
+	def apply_nr(self,n,r)
+		#not used?
+		'''
+		xDist = n["x"] - r.config('massCenterX')
+		yDist = n["y"] - r.config('massCenterY')
+		distance = math.sqrt(xDist * xDist + yDist * yDist)
+		if (distance > 0):
+			factor = self.coefficient * n["fa2"]["mass"] * r.config('mass') /math.pow(distance, 2)
+			n["fa2"]["dx"] += xDist * factor
+			n["fa2"]["dy"] += yDist * factor
+		elif(distance<0):
+			factor = -self.coefficient * n["fa2"]["mass"] * r.getMass() / distance
+			n["fa2"]["dx"] += xDist * factor
+			n["fa2"]["dy"] += yDist * factor
+		'''
+
+	def apply_g(self,n,g)
+		xDist = n["x"]
+		yDist = n["y"]
+		distance = math.sqrt(xDist*xDist + yDist*yDist)
+		if (distance > 0):
+          	#NB: factor = force / distance
+			factor = self.coefficient * n["fa2"]["mass"] * g / distance
+			n["fa2"]["dx"] -= xDist * factor
+			n["fa2"]["dy"] -= yDist * factor
 
 class strongGravity:
 	def __init__(self,c):
 		self.coefficient = c
 
+	def apply_nn(self,n1,n2):
+		pass
+
+	def apply_nr(self,n,r)
+		pass
+
+	def apply_g(self,n,g)
+		xDist = n["x"]
+		yDist = n["y"]
+		distance = math.sqrt(xDist*xDist + yDist*yDist)
+		if (distance > 0):
+          	#NB: factor = force / distance
+			factor = self.coefficient * n["fa2"]["mass"] * g
+			n["fa2"]["dx"] -= xDist * factor
+			n["fa2"]["dy"] -= yDist * factor
 
 
+class linAttraction:
+	def __init__(self,c):
+		self.coefficient=c
 
+	def apply_nn(self,n1, n2, e):
+		if n1["fa2"] and n2["fa2"]:
+			xDist = n1["x"] - n2["x"]
+			yDist = n1["y"] - n2["y"]
+			factor = -self.coefficient * e
 
+			n1["fa2"]["dx"] += xDist * factor
+			n1["fa2"]["dy"] += yDist * factor
+			n2["fa2"]["dx"] -= xDist * factor
+			n2["fa2"]["dy"] -= yDist * factor
 
+class linAttraction_massDistributed:
+	def __init__(self,c):
+		self.coefficient=c
 
+	def apply_nn(self,n1, n2, e):
+		if n1["fa2"] and n2["fa2"]:
+			xDist = n1["x"] - n2["x"]
+			yDist = n1["y"] - n2["y"]
+			factor = -self.coefficient * e / n1["fa2"]["mass"]
 
+			n1["fa2"]["dx"] += xDist * factor
+			n1["fa2"]["dy"] += yDist * factor
+			n2["fa2"]["dx"] -= xDist * factor
+			n2["fa2"]["dy"] -= yDist * factor
 
+class logAttraction:
+	def __init__(self,c):
+		self.coefficient=c
 
+	def apply_nn(self,n1, n2, e):
+		if n1["fa2"] and n2["fa2"]:
+			xDist = n1["x"] - n2["x"]
+			yDist = n1["y"] - n2["y"]
+			distance = math.sqrt(xDist * xDist + yDist * yDist)
 
+			if (distance > 0):
+			#NB: factor = force / distance
+				factor = -self.coefficient * e * math.log(1 + distance)/distance
+				n1["fa2"]["dx"] += xDist * factor
+				n1["fa2"]["dy"] += yDist * factor
+				n2["fa2"]["dx"] -= xDist * factor
+				n2["fa2"]["dy"] -= yDist * factor
+
+class logAttraction_degreeDistributed:
+	def __init__(self,c):
+		self.coefficient=c
+
+	def apply_nn(self,n1, n2, e):
+		if n1["fa2"] and n2["fa2"]:
+			xDist = n1["x"] - n2["x"]
+			yDist = n1["y"] - n2["y"]
+			distance = math.sqrt(xDist * xDist + yDist * yDist)
+
+			if (distance > 0):
+			#NB: factor = force / distance
+				factor = -self.coefficient * e * math.log(1 + distance)/distance/n1["fa2"]["mass"]
+				n1["fa2"]["dx"] += xDist * factor
+				n1["fa2"]["dy"] += yDist * factor
+				n2["fa2"]["dx"] -= xDist * factor
+				n2["fa2"]["dy"] -= yDist * factor
+
+class linAttraction_antiCollision:
+	def __init__(self,c):
+		self.coefficient=c
+
+	def apply_nn(self,n1, n2, e):
+		if n1["fa2"] and n2["fa2"]:
+			xDist = n1["x"] - n2["x"]
+			yDist = n1["y"] - n2["y"]
+			distance = math.sqrt(xDist * xDist + yDist * yDist)
+
+			if (distance > 0):
+			#NB: factor = force / distance
+				factor = -self.coefficient * e
+				n1["fa2"]["dx"] += xDist * factor
+				n1["fa2"]["dy"] += yDist * factor
+				n2["fa2"]["dx"] -= xDist * factor
+				n2["fa2"]["dy"] -= yDist * factor
+
+class linAttraction_degreeDistributed_antiCollision:
+	def __init__(self,c):
+		self.coefficient=c
+
+	def apply_nn(self,n1, n2, e):
+		if n1["fa2"] and n2["fa2"]:
+			xDist = n1["x"] - n2["x"]
+			yDist = n1["y"] - n2["y"]
+			distance = math.sqrt(xDist * xDist + yDist * yDist)
+
+			if (distance > 0):
+			#NB: factor = force / distance
+				factor = -self.coefficient * e / n1["fa2"]["mass"];
+				n1["fa2"]["dx"] += xDist * factor
+				n1["fa2"]["dy"] += yDist * factor
+				n2["fa2"]["dx"] -= xDist * factor
+				n2["fa2"]["dy"] -= yDist * factor
+
+class logAttraction_antiCollision:
+	def __init__(self,c):
+		self.coefficient=c
+
+	def apply_nn(self,n1, n2, e):
+		if n1["fa2"] and n2["fa2"]:
+			xDist = n1["x"] - n2["x"]
+			yDist = n1["y"] - n2["y"]
+			distance = math.sqrt(xDist * xDist + yDist * yDist)
+
+			if (distance > 0):
+			#NB: factor = force / distance
+				factor = -self.coefficient * e * math.log(1 + distance)/distance
+				n1["fa2"]["dx"] += xDist * factor
+				n1["fa2"]["dy"] += yDist * factor
+				n2["fa2"]["dx"] -= xDist * factor
+				n2["fa2"]["dy"] -= yDist * factor
+
+class logAttraction_degreeDistributed_antiCollision:
+	def __init__(self,c):
+		self.coefficient=c
+
+	def apply_nn(self,n1, n2, e):
+		if n1["fa2"] and n2["fa2"]:
+			xDist = n1["x"] - n2["x"]
+			yDist = n1["y"] - n2["y"]
+			distance = math.sqrt(xDist * xDist + yDist * yDist)
+
+			if (distance > 0):
+			#NB: factor = force / distance
+				factor = -self.coefficient * e * math.log(1 + distance)/distance/n1["fa2"]["mass"]
+				n1["fa2"]["dx"] += xDist * factor
+				n1["fa2"]["dy"] += yDist * factor
+				n2["fa2"]["dx"] -= xDist * factor
+				n2["fa2"]["dy"] -= yDist * factor
 
 
 
